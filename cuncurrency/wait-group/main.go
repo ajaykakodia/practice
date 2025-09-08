2044aadd9ba2e07831ee450de1097ab136b2a390
Ajay Yadav <ajay.kakodia@gmail.com>
2025-09-08 14:58:52 +0530

    program added for waitGroup

---
+++ cuncurrency/wait-group/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("OS\t\t", runtime.GOOS)
+	fmt.Println("ARCH\t\t", runtime.GOARCH)
+	fmt.Println("CPU\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine())
+
+	wg.Add(2)
+	go foo()
+	go bar()
+
+	fmt.Println("CPU\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t\t", runtime.NumGoroutine())
+	wg.Wait()
+}
+
+func foo() {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func bar() {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
