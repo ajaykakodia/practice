cbebcf5a6aa0dca11db6b86260adb1fab5334dae
Ajay Yadav <ajay.kakodia@gmail.com>
2025-09-09 16:11:18 +0530

    adding pipeline program

---
+++ cuncurrency/channels/17-pipeline/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := gen(3, 4)
+	sqc := sq(ch)
+	fmt.Println(<-sqc) // 9
+	fmt.Println(<-sqc) // 16
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, num := range nums {
+			out <- num
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(c <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for val := range c {
+			out <- val * val
+		}
+		close(out)
+	}()
+	return out
+}
