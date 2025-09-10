05982fa88f6eb1d7fd4ddebae067c61879433df6
Ajay Yadav <ajay.kakodia@gmail.com>
2025-09-10 23:15:33 +0530

    factorial challenge

---
+++ cuncurrency/channels/21-deadlock-challange/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	in := gen()
+
+	xc := fanOut(in, 10)
+
+	fmt.Println("%T \n", xc)
+	fmt.Println("********************", len(xc))
+	for _, v := range xc {
+		fmt.Println("*************", <-v)
+	}
+
+	for n := range merge(xc...) {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanOut(in <-chan int, n int) []<-chan int {
+	xc := make([]<-chan int, n)
+	for i := 0; i < n; i++ {
+		xc[i] = factorial(in)
+	}
+	return xc
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
