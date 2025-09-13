1aa2586bb5d2ee85b981da27dad5441b075333d9
Ajay Yadav <ajay.kakodia@gmail.com>
2025-09-13 13:23:43 +0530

    adding table test

--- testing/func/main_test.go
+++ testing/func/main_test.go
@@ -28,3 +28,47 @@ func TestMySumWrong(t *testing.T) {
 		t.Error("Expected", expected, " but Got", actual)
 	}
 }
+
+func TestMySumTableTest(t *testing.T) {
+	type test struct {
+		data           []int
+		expectedOutput int
+	}
+
+	tests := []test{
+		{data: []int{21, 21}, expectedOutput: 42},
+		{data: []int{10, 10, 22}, expectedOutput: 42},
+		{data: []int{10, 10, 10, 12}, expectedOutput: 42},
+		{data: []int{5, 5, 5, 5, 6, 6, 10}, expectedOutput: 42},
+		{data: []int{-1, -1, 2}, expectedOutput: 0},
+	}
+
+	for _, val := range tests {
+		result := mySum(val.data...)
+		if result != val.expectedOutput {
+			t.Error("Expected", val.expectedOutput, "but Got", result)
+		}
+	}
+}
+
+func TestMySumWrongTableTest(t *testing.T) {
+	type test struct {
+		data           []int
+		expectedOutput int
+	}
+
+	tests := []test{
+		{data: []int{21, 21}, expectedOutput: 42},
+		{data: []int{10, 10, 22}, expectedOutput: 42},
+		{data: []int{10, 10, 10, 12}, expectedOutput: 42},
+		{data: []int{5, 5, 5, 5, 6, 6, 10}, expectedOutput: 42},
+		{data: []int{-1, -1, 2}, expectedOutput: 0},
+	}
+
+	for _, val := range tests {
+		result := mySumWrong(val.data...)
+		if result != val.expectedOutput {
+			t.Error("Expected", val.expectedOutput, "but Got", result)
+		}
+	}
+}
